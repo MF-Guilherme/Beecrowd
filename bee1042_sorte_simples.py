0f13779c3c1f40b078d061bbc77bbb903defd118
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2023-04-25 17:08:10 -0300

    1042 Sorted simples, resolvido

---
+++ bee1042_sorte_simples.py
@@ -0,0 +1,11 @@
+n1, n2, n3 = map(int, input().split())
+
+lista_padrao = [n1, n2, n3]
+lista_ordenada = sorted(lista_padrao)
+
+for i in lista_ordenada:
+    print(i)
+print()
+for j in lista_padrao:
+    print(j)
+

882aeaaed189159c4f0d938bb9d44ead63ed382a
MF-Guilherme <guimontenegro23@yahoo.com.br>
2023-11-22 17:54:38 -0300

    1099 soma de impares consecutivos 2 - aceito

--- Iniciante/bee1099_soma_de_impares_consecutivos2.py
+++ Iniciante/bee1099_soma_de_impares_consecutivos2.py
@@ -1,10 +1,25 @@
-impares = []
+def soma_impares(n1, n2):
+    lista = []
+    for j in range(n1, n2+1):
+        lista.append(j)
+    del(lista[0])
+    del(lista[-1])
+    if len(lista) == 0:
+        print(0)
+    else:
+        impares = []
+        for item in lista:
+            if item % 2 != 0:
+                impares.append(item)
+        print(sum(impares))
+
 testes = int(input())
 
 for i in range(testes):
     x, y = map(int, input().split(' '))
-    if x % 2 != 0:
-        impares.append(x)
-    if y % 2 != 0:
-        impares.append(y)
-    
+    if x == y:
+        print(0)
+    elif x < y:
+        soma_impares(x, y)
+    else:
+        soma_impares(y, x)

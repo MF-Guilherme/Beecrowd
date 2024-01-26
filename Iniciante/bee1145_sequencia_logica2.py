f3d845a669e6724f92e8b5e14b4395694a8dee82
MF-Guilherme <guimontenegro23@yahoo.com.br>
2024-01-26 18:01:57 -0300

    1145 - Sequencia logica 2 resolvido

---
+++ Iniciante/bee1145_sequencia_logica2.py
@@ -0,0 +1,20 @@
+def seq_log(x, y):
+    cont = 1
+    while cont <= y:
+        for i in range(1, x + 1):
+            if i == x:
+                print(cont)
+                cont += 1
+            else:
+                print(cont, end=' ')
+                cont += 1
+            
+
+
+x, y = map(int, input().split(' '))
+
+seq_log(x, y)
+
+
+
+        
